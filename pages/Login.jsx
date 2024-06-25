751bc5314d9093427583fc5e947fd5252ab67728
Ronaldo Gandra Rocha <ronaldogandrarocha@gmail.com>
2024-06-25 15:20:35 -0300

    criado componente de imagem

---
+++ pages/Login.jsx
@@ -0,0 +1,14 @@
+import ImageWithSpace from '../src/components/layout/ImageWithSpace'
+
+
+function LoginPage () {
+  return (
+    <div>
+      <ImageWithSpace>
+         Teste 123
+      </ImageWithSpace>
+    </div>
+  )
+}
+
+export default LoginPage
