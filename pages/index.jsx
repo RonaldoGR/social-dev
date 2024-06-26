71a8baee0a3eaaf40e3d001c5de0e918800aaed5
Ronaldo Gandra Rocha <ronaldogandrarocha@gmail.com>
2024-06-26 09:08:40 -0300

    criado componente de criar post

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,24 @@
+import styled from 'styled-components'
 import NavBar from "../src/components/layout/NavBar"
+import Container from '../src/components/layout/Container'
+import CreatePost from '../src/components/cards/CreatePost'
 
+
+const Content = styled.div`
+  margin: 50px 0;
+`
 
 function HomePage () {
   
   return (
   <>
     <NavBar />
-    <div>
-      SejaDev
-    </div>
+    <Content>
+      <Container>
+        <CreatePost />
+      </Container>
+
+    </Content>
   </>
   )
 }
